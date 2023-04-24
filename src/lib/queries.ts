fa4ae3d729c21c240ac99733b028cf1194b4ddb8
Yukio <yukio5347@gmail.com>
2023-04-24 12:12:15 +0900

    add src/lib/queries.ts

---
+++ src/lib/queries.ts
@@ -0,0 +1,33 @@
+import { ParsedUrlQuery } from 'querystring';
+
+import { serialize } from '@/lib/helpers';
+import { prisma } from '@/lib/prisma';
+
+export const cardStaticProps = async (params: ParsedUrlQuery | undefined) => {
+  const id = Number(params?.id);
+  const card = serialize(
+    await prisma.card.findUnique({
+      where: {
+        id: id,
+      },
+    }),
+  );
+  return {
+    props: { card },
+  };
+};
+
+export const cardStaticPaths = async () => {
+  const cards = await prisma.card.findMany({ select: { id: true } });
+  const paths = cards.map((card) => {
+    return {
+      params: {
+        id: String(card.id),
+      },
+    };
+  });
+  return {
+    paths: paths,
+    fallback: false,
+  };
+};
