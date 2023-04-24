9ed455dd9a3afc30c74476c3d7bebcad9b00d79d
Yukio <yukio5347@gmail.com>
2023-04-24 10:00:17 +0900

    move useEffect from _app.tsx to Alert component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,10 @@
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
 
-import { AlertProvider, useAlertContext } from '@/components/Alert';
+import { AlertProvider } from '@/components/Alert';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { type, message, hideAlert } = useAlertContext();
-
-  useEffect(() => {
-    if (message && type !== 'warning' && type !== 'error') {
-      setTimeout(() => {
-        hideAlert();
-      }, 3000);
-    }
-  }, [message]);
-
   return (
     <AlertProvider>
       <Component {...pageProps} />
